d401a58e43dc1ffb904657a45ce9bdb46d8ba67b
SMalpica <sandra_malpica@hotmail.com>
2016-03-24 16:05:37 +0100

    Iniciando sincronizacion a 4

--- SphereWars/src/videogame/Main.java
+++ SphereWars/src/videogame/Main.java
@@ -1,5 +1,17 @@
 package videogame;
-
+/**
+ * Autores: Adrian,
+ * 			Juan Luis,
+ * 			Sandra Malpica Mallo - 670607,
+ * 			Richard
+ * 	TODO: rellenar con apellidos y NIP (cada uno el suyo para comprobar que va bien esto)
+ * 	TODO: crear una rama para cada uno. SUBIR A MASTER SOLO LO QUE VAYA BIEN.
+ * 	
+ * Clase: Main.java
+ * 
+ * Comentarios: Clase principal del videojuego
+ * 
+ * */
 public class Main {
 
 }
