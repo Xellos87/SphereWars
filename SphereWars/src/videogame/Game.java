653c42b222e4f70b3c70916e8a3bec68c5774752
JuanLuis <juanluis1204@gmail.com>
2016-04-05 17:26:50 +0200

    Añadida base 2d

--- SphereWars/src/videogame/Game.java
+++ SphereWars/src/videogame/Game.java
@@ -1,5 +1,68 @@
 package videogame;
 
-public class Game {
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements Runnable {
+	
+	//Dimensiones del juego
+	private static final int WIDTH = 320;
+	private static final int HEIGHT = 240;	
+	private static int SCALE = 2;	
+	
+	//Thread del juego
+	private boolean running;
+	private int FPS = 60;
+	private int targetTime = 1000/FPS; //ms
+	private Thread thread;
+	
+	//Imagen del juego
+	private BufferedImage image;
+	private Graphics2D g;
+
+	public Game(){
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setFocusable(true);
+		requestFocus();
+	}
+	
+	public void init() {
+		running = true;
+		image = new BufferedImage(WIDTH * SCALE, HEIGHT * SCALE, 
+				BufferedImage.TYPE_INT_RGB);
+		if(thread == null){
+			thread = new Thread(this);
+			//TODO: añadir keyListener
+			thread.start();
+		}
+		
+	}
+	
+	@Override
+	public void run() {
+		//Tiempos de control de bucle
+		long start;
+		long elapsed;
+		long wait;
+		
+		while(running){
+			start = System.currentTimeMillis();
+			
+			//TODO: actualización del juego
+			
+			elapsed = System.currentTimeMillis() - start;
+			wait = targetTime - elapsed;
+			//Duerme el hilo hasta la siguiente actualización
+			try {
+				Thread.sleep(wait);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			};
+		}
+	}
 
 }
--- SphereWars/src/videogame/Main.java
+++ SphereWars/src/videogame/Main.java
@@ -1,4 +1,9 @@
 package videogame;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 /**
  * Autores: Victor Adrian Milla Español - 557022,
  * 			Juan Luis Burillo Ortín - 542083,
@@ -11,5 +16,22 @@ package videogame;
  * 
  * */
 public class Main {
+	
+	private static JFrame window;
+	private static final String TITLE = "Sphere Wars";
 
+	public static void main(String[] args){
+		window = new JFrame(TITLE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(new BorderLayout());
+		window.setResizable(false);
+		window.setVisible(true);
+
+		Game game = new Game();
+		game.init();
+		window.add(game, BorderLayout.CENTER);
+		//Ajusta el tamaño de la ventana según los componentes
+		window.pack();
+		
+	}	
 }
