defab6227c510e2ecdda931eeb92d9d7d114cd4c
axteroide <adrianmilla@gmail.com>
2016-05-29 17:55:40 +0200

    Add files via upload

--- SphereWars/src/videogame/Game.java
+++ SphereWars/src/videogame/Game.java
@@ -120,12 +120,12 @@ public class Game extends JLayeredPane{
 		//Inicializa el juego
 		if(mode == MODE_2D){
 			//Inicio de juego 2D, primer jugador
-			game2d_1p = new Game2D(width, height_game,num_players,map_p1.getWidthBlock(),map_p1.getHeightBlock());
+			game2d_1p = new Game2D(width, height_game,num_players,map_p1.getWidthBlock(),map_p1.getHeightBlock(), main.getPanel());
 			game2d_1p.setBounds(0, height_score, width, height_game);
 			add(game2d_1p, new Integer(0),3);
 			if(num_players>1){
 				//Segundo jugador si lo hay
-				game2d_2p = new Game2D(width, height_game,num_players,map_p1.getWidthBlock(),map_p1.getHeightBlock());
+				game2d_2p = new Game2D(width, height_game,num_players,map_p1.getWidthBlock(),map_p1.getHeightBlock(), main.getPanel());
 				game2d_2p.setBounds(0, height_score+height_game, width, height_game);
 				add(game2d_2p, new Integer(0),4);
 			}
@@ -297,7 +297,7 @@ public class Game extends JLayeredPane{
 				}
 			}
 		}else{
-			if(num_players == 1 && (e.getKeyCode() == Constants.teclaSaltop1 || e.getKeyCode() == Constants.teclaSprintp1)){
+			if(e.getKeyCode() == Constants.teclaSaltop1 || e.getKeyCode() == Constants.teclaSprintp1){
 				if(mode == MODE_2D){
 					System.out.println("Tecla pulsada");
 					game2d_1p.keyPressed(e);
@@ -309,6 +309,8 @@ public class Game extends JLayeredPane{
 				if(mode == MODE_2D){
 					System.out.println("Tecla pulsada");
 					game2d_2p.keyPressed(e);
+				}else if(mode == MODE_3D){
+
 				}
 			}
 		}
