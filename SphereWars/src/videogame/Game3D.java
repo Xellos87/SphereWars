2b5778278e5667e788700036cf7e8f91ca06996f
Xellos <Xellos87@gmail.com>
2016-06-01 02:04:05 +0200

    Posiciona la camara 3D segun la resolucion

--- SphereWars/src/videogame/Game3D.java
+++ SphereWars/src/videogame/Game3D.java
@@ -162,7 +162,7 @@ public class Game3D extends Canvas3D implements KeyListener{
 		Transform3D home=new Transform3D();
 		simpleU.getViewingPlatform().getViewPlatformTransform().getTransform(home);
 		//System.out.println(home.toString());
-		double[] homeM = {1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.4, 0.0, 0.0, 1.0, 2.41421356, 0.0, 0.0, 0.0, 1.0};
+		double[] homeM = {1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.4, 0.0, 0.0, 1.0, 2.41421356/2*Constants.scale, 0.0, 0.0, 0.0, 1.0};
 		Transform3D homeT = new Transform3D(homeM);
 		orbit.setHomeTransform(homeT);		
 		ViewingPlatform vp = simpleU.getViewingPlatform();
