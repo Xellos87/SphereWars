a7551c7330454d9302c567951569606b887379fb
Xellos <Xellos87@gmail.com>
2016-06-02 05:26:05 +0200

    GameObject comentado

--- SphereWars/src/videogame/GameObject.java
+++ SphereWars/src/videogame/GameObject.java
@@ -22,10 +22,10 @@ import com.sun.j3d.utils.geometry.Primitive;
  *
  */
 public class GameObject {
-	//Posici�n en pantalla
+	//Posición en pantalla
 	protected int x;
 	protected int y;
-	protected int z;//TODO, entero o flotantes para 3D
+	protected int z;
 	//Velocidad de movimiento
 	protected int vx;
 	protected int vy;
@@ -41,10 +41,9 @@ public class GameObject {
 	//Disemnsiones de la imagen representado en pantalla(para colisiones, mas realista)
 	protected int real_block_width;
 	protected int real_block_height;
-	//Posici�n real de la x e y dentro del bloque
+	//Posición real de la x e y dentro del bloque
 	protected int real_x_block;
 	protected int real_y_block;
-	
 	//Sprite
 	protected BufferedImage image;
 	protected BufferedImage texture;	
@@ -52,13 +51,22 @@ public class GameObject {
 	protected TransformGroup tg_model3D;
 	protected BranchGroup branch_group;
 	protected Primitive object_primitive;
-	
-	
 	//Variables de colisiones
 	protected boolean kills;
 	
 	
-	
+	/**
+	 * Constructor
+	 * 
+	 * @param x, posición del objeto
+	 * @param y, posición del objeto
+	 * @param x_img, posición en el contenedor para cargar la imagen
+	 * @param y_img, posición en el contenedor para cargar la imagen
+	 * @param width, ancho de la imagen en el contenedor
+	 * @param height, alto de la imagen en el contenedor
+	 * @param block_width, ancho de bloque en pantalla
+	 * @param block_height, alto de bloque en pantalla
+	 */
 	public GameObject(int x, int y,int x_img,int y_img, int width, int height,int block_width,int block_height){
 		this.x = x;
 		this.y = y;
@@ -73,6 +81,9 @@ public class GameObject {
 		this.block_height = block_height;
 	}
 	
+	/**
+	 * Redimensiona el objeto
+	 */
 	protected void resize(){
 		//Comprobaciones por si la imagen no es cuadrada
 		int w;
@@ -101,63 +112,128 @@ public class GameObject {
 		//System.out.printf("Nuevo tamaño, ancho: %d, alto: %d\n",image.getWidth(),image.getHeight());
 	}
 	
+	/**
+	 * Establece la posición
+	 * 
+	 * @param x, posición nueva de la x
+	 * @param y, posición nueva de la y
+	 */
 	public void setPosition(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Establece la velocidad
+	 * 
+	 * @param vx, nueva velocidad de la x
+	 * @param vy, nueva velocidad de la y
+	 */
 	public void setVelocity(int vx, int vy){
 		this.vx = vx;
 		this.vy = vy;
 	}
 	
+	/**
+	 * Mueve el objeto
+	 */
 	public void move(){
 		setPosition(x+vx, y+vy);
 	}
 	
-	//TODO: mejorar intersecciones(esquinas)
+	/**
+	 * Comprueba la intersección del objeto con otro
+	 * 
+	 * @param o, objeto con el que se comprueba la intersección
+	 * @param x_ori, x desde la que se dibuja
+	 * @param y_ori, y desde la que se dibuja
+	 * @return true si y solo si hay intersección entre los objetos
+	 */
 	public boolean intersects(GameObject o,int x_ori, int y_ori){
-		Rectangle r = getBox(x_ori,y_ori).intersection(o.getBox(x_ori,y_ori));
+		//Rectangle r = getBox(x_ori,y_ori).intersection(o.getBox(x_ori,y_ori));
 		//System.out.println(r.toString());
 		return getBox(x_ori,y_ori).intersects(o.getBox(x_ori,y_ori));		
 	}
 	
+	/**
+	 * Obtiene la caja de colisión
+	 * 
+	 * @param x_ori, x desde la que se dibuja
+	 * @param y_ori, y desde la que se dibuja
+	 * @return el rectangulo de colision
+	 */
 	public Rectangle getBox(int x_ori, int y_ori){
 		return new Rectangle(x_ori+x+real_x_block, y_ori+y+real_y_block, real_block_width, real_block_height);
 	}
 
+	/**
+	 * Actualiza la posición x
+	 * @param x, nuevo valor
+	 */
 	public void updatePositionX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * 
+	 * @return la anchura
+	 */
 	public int getWidth(){
 		return width;
 	}
 
+	/**
+	 * 
+	 * @return la altura
+	 */
 	public int getHeight(){
 		return height;
 	}
 	
+	/**
+	 * 
+	 * @return ancho de pantalla
+	 */
 	public int getWidthScreen(){
 		return block_width;
 	}
 
+	/**
+	 * 
+	 * @return alto de pantalla
+	 */
 	public int getHeightScreen(){
 		return block_height;
 	}
 	
+	/**
+	 * 
+	 * @return posición de la x
+	 */
 	public int getPositionX(){
 		return x;
 	}
 	
+	/**
+	 * 
+	 * @return posición de la y
+	 */
 	public int getPositionY(){
 		return y;
 	}
 	
+	/**
+	 * 
+	 * @return si el objeto mata al jugador
+	 */
 	public boolean kills(){
 		return kills;
 	}
 	
+	/**
+	 * 
+	 * @return el modelo 3D del objeto
+	 */
 	public TransformGroup get3DModel() {
 		return tg_model3D;
 	}
